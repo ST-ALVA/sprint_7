58cf41bb43de009549d85a1cb5af4859cc33e6a9
Stephan Thions Alvarez <sthions7@gmail.com>
2025-03-22 21:22:48 -0700

    Initial setup for EDA Web App

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sprint_7
 TripleTen Sprint 7: Exploratory data analysis in web using streamlit and render
+
+Este es un analisis exploratorio con datos de vehiculos.
+
+El usuario puede escoger crear un histograma con los datos de modelo de los vehiculos en el dataset, o bien un diagrama de dispersion para visualizar la relacion entre cilindros y años de modelo de los vehiculos.
---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import streamlit as st 
+import plotly.express as px 
+
+# Load the data
+vehicle_df = pd.read_csv('/Users/stephanethions/Documents/GitHub/sprint_7/vehicles_us.csv')
+
+# Display the data
+print(vehicle_df.head())
+vehicle_df.info()
+
+# Display a header title for the web app
+st.header('Vehicle Dataset')
+
+# Crear una casilla de verificacion
+build_histogram = st.checkbox('Build Odometer Histogram')
+
+# Create an Histogram triggered by the checkbox showing the model column data
+if build_histogram:
+    fig = px.histogram(vehicle_df, x='model', nbins=50)
+    st.plotly_chart(fig, use_container_width=True)
+
+# Scatter Plot Button
+scatter_button = st.button('Show Scater Plot')
+
+# Creates a Scatter Plot trigerred by the above button showing the relation between model_year and vehicle cylinders
+if scatter_button:
+    fig = px.scatter(vehicle_df, x='model_year', y='cylinders')
+    st.plotly_chart(fig, use_container_width=True)
+
